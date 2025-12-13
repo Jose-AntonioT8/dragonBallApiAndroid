23d50fa75ad52cf619d87ccfe1defa4339e82358
José Antonio Domínguez gonzález <joseantdominguezgonzalez@gmail.com>
2025-12-13 17:04:08 +0100

    sdadsa

--- app/src/main/java/com/example/dragonballapiandroid/data/local/PlanetDataBase.kt
+++
@@ -1,9 +0,0 @@
-package com.example.dragonballapiandroid.data.local
-
-//import androidx.room.Database
-//import androidx.room.RoomDatabase
-//
-//@Database(entities= [CharacterEntity::class], version =1)
-//abstract class PlanetDataBase(): RoomDatabase() {
-//    abstract fun getPlanetDao(): PlanetDao
-//}
